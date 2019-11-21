2cc9e053710c48a205f0ed0d6e5502090955f522
lngach <nam.le@gumiviet.com>
2019-11-21 13:43:29 +0700

    #Add prod.secret

---
+++ config/prod.secret.exs
@@ -0,0 +1,7 @@
+use Mix.Config
+
+# In this file, we keep production configuration that
+# you likely want to automate and keep it away from
+# your version control system.
+config :elixir_stream, ElixirStream.Endpoint,
+  secret_key_base: "MUJ4r0EHKHteNSsyTCFjXQOE0JG/7ap25FmNHsdCHjeJip2DA2YNnt4DiLNTy5XH"
